a810fcfcf87dde63746152da102b81dc3c4ed9b1
Alexey Berezutsky <AlexeyBerezutsky@gmail.com>
2022-09-08 16:33:16 +0300

    small fix on link with auth

--- components/LinkWithAuth.js
+++ components/LinkWithAuth.js
@@ -4,14 +4,13 @@ import Link from 'next/link';
 export default function LinkWithAuth({ children }) {
     const { user, setUser, signout } = useAuth();
 
-    const href = user ? '/welcome' : process.env.NEXT_PUBLIC_AUTH;
+    // const href = user ? '/welcome' : process.env.NEXT_PUBLIC_AUTH;
 
     return (
-        <>
-            <Link passHref href={href}>
+        <Link passHref href={'/welcome'}>
+            <div onClick={() => { !user ? setUser({ name: 'Alexey' }) : signout() }}>
                 {children}
-            </Link>
-            <button onClick={() => { !!user ? setUser({ name: 'Alexey' }) : signout() }}>{!!user ? 'LogOutTest' : 'LogInTest'}</button>
-        </>
+            </div>
+        </Link>
     );
 }
