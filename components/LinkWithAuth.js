302611f182163ff25d38252b6acfb91e1d3ec707
Alexey Berezutsky <AlexeyBerezutsky@gmail.com>
2022-09-08 10:08:09 +0300

    fix auth linl WIP

--- components/LinkWithAuth.js
+++ components/LinkWithAuth.js
@@ -1,14 +1,17 @@
-import {useAuth} from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth';
 import Link from 'next/link';
 
-export default function LinkWithAuth({children}) {
-    const {user} = useAuth();
+export default function LinkWithAuth({ children }) {
+    const { user, setUser, signout } = useAuth();
 
     const href = user ? '/welcome' : process.env.NEXT_PUBLIC_AUTH;
 
     return (
-        <Link passHref href={href}>
-            {children}
-        </Link>
+        <>
+            <Link passHref href={href}>
+                {children}
+            </Link>
+            <button onClick={() => { !!user ? setUser({ name: 'Alexey' }) : signout() }}>{!!user ? 'LogOutTest' : 'LogInTest'}</button>
+        </>
     );
 }
--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -1,65 +1,49 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import jwtDecode from '../helpers/jwtDecode';
+import { sleep } from '../helpers/tools';
 import { useAuth } from '../hooks/useAuth';
 
+const protectedPaths = ['/welcome/', '/welcome'];
+const publicPaths = ['/', '/privacy-policy/', '/about/'];
+
+const routeCheck = (url, isLoggein) => {
+    const path = url.split(/[#,?]/)[0];
+
+    if (publicPaths.includes(path) || (protectedPaths.includes(path) && isLoggein)) {
+        return path
+    }
+
+    return '/'
+}
+
 export default function RouteGuard({ children }) {
-    const { user, setUser, remove } = useAuth();
+    const { user } = useAuth();
     const router = useRouter();
     const [hide, setHide] = useState(true);
-
-    function authCheck(url) {
-        const path = url.split(/[#,?]/)[0];
-        const protectedPaths = ['/welcome/', '/welcome'];
-        const publicPaths = ['/', '/privacy-policy/', '/about/'];
-
-        if (!protectedPaths.includes(path)) {
-            if (!publicPaths.includes(path)) {
-                return router.push('/');
-            }
-            return Promise.resolve();
-        } else {
-            if (!user) {
-                const token = url.split('#id_token=')[1];
-                if (token) {
-                    const decodedToken = jwtDecode(token);
-                    setUser(decodedToken);
-                    return router.push(path);
-                } else {
-                    return router.push('/');
-                }
-            }
-
-            const error = url.split('#error')[1];
-            if (error) {
-                remove();
-                return router.push('/');
-            }
-        }
-
-        return Promise.resolve();
-    }
 
     const beforeStart = () => {
         setHide(true);
     };
 
-    const afterComplete = (url) => {
-        authCheck(url).then(() => {
-            setHide(false);
-        });
+    const afterComplete = async (url) => {
+        setHide(false);
+
+        const newRoute = routeCheck(url, !!user);
+        if(newRoute !== url) {
+            router.push(newRoute);
+        }
     };
 
     useEffect(() => {
-        authCheck(router.asPath).then(() => {
-            setHide(false);
-        });
+        (async () => {
+            await afterComplete(router.asPath);
+        })()
 
         router.events.on('routeChangeStart', beforeStart);
 
         router.events.on('routeChangeComplete', afterComplete);
 
-        // unsubscribe from events in useEffect return function
         return () => {
             router.events.off('routeChangeStart', beforeStart);
             router.events.off('routeChangeComplete', afterComplete);
