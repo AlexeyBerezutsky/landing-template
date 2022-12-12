1bcac3f3f6007f1fc1e8a8fa031c943eca983495
Alexey Berezutsky <aberezutsky@systemorph.com>
2022-12-12 23:37:30 +0300

    intro as separate scss module

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import styles from "./intro.module.scss";
 import { IntroImage } from "./IntroImage";
+import classNames from "classnames";
 
 export function Intro() {
     return (
-        <section className={"intro"}>
-            <div className="intro__filter-layer intro__filter-layer--1"></div>
-            <div className="intro__filter-layer intro__filter-layer--2"></div>
+        <section className={styles.intro}>
+            <div className={classNames(styles.intro__filterLayer, styles.intro__filterLayer1)}/>
+            <div className={classNames(styles.intro__filterLayer, styles.intro__filterLayer2)}/>
             {/* <video className="intro__video" autoPlay loop muted preload="auto" poster="/img/poster.jpg">
                 <source src="/img/landing-video.mp4" type="video/mp4" />
             </video> */}
-            <div className="container-fluid intro__container">
+            <div className={classNames("container-fluid", styles.intro__container)}>
                 <div className="row">
                     <div className="col-12">
-                        <h1 className="intro__heading">
+                        <h1 className={styles.intro__heading}>
                             Nunc sodales lectus
-                            <br />
+                            <br/>
                             Vestibulum. Nunc. Mauris.
                         </h1>
-                        <p className="intro__text">
+                        <p className={styles.intro__text}>
                             Fusce vel mollis nunc, ac faucibus mi. Nunc turpis tortor, congue eu finibus eu, ultricies
                             ac diam. Aenean laoreet, ex eget bibendum bibendum, sem leo dapibus odio, at semper mi lacus
                             ut urna. In fermentum purus in mollis facilisis. Sed eget orci ullamcorper felis molestie
